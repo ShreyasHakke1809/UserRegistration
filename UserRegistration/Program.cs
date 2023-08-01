17c29ec290c22a319e2ef94035f4214403c8dcf6
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-01 20:29:47 +0530

    Creating a user registration program using Regx

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registrtion program");
+        }
+    }
+}
