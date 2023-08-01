930019ca1cb37cc96164550e537c2a2313c09a5a
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-01 20:38:19 +0530

    Creating valid first name project

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registrtion program");
+            UserReg.ValidFirstName();
         }
     }
 }
---
+++ UserRegistration/UserReg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class UserReg
+    {
+        public static void ValidFirstName()
+        {
+            Console.WriteLine("\nPlease Enter your First Name:");
+            string firstName = Console.ReadLine();
+            string firstName_pattern = "^[A-Z][a-zA-Z]{2,}$";
+
+            if (Regex.IsMatch(firstName, firstName_pattern))
+            {
+                Console.WriteLine("First Name is Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Valid");
+                ValidFirstName();
+            }
+        }
+    }
+}
