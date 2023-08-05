b691bc55204baccedfd0eeb6743e4dc3a0615eec
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-05 11:18:00 +0530

    Adding password having atleast 1 upper case program

--- UserRegistration/UserReg.cs
+++ UserRegistration/UserReg.cs
@@ -77,8 +77,7 @@ namespace UserRegistration
             Console.WriteLine("\nPlease Enter your Password:");
             Console.WriteLine("Password must contain min 8 characters");
             string password = Console.ReadLine();
-            string password_pattern = "^[a-zA-Z]{8,}$";
-
+            string password_pattern = "^(?=.*[a-z])(?=.*[A-Z]).{8,}$";
             if (Regex.IsMatch(password, password_pattern))
             {
                 Console.WriteLine("Password is Valid");
